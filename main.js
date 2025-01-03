5abc28843e15e6c54617d051a1e9eb30d1fb2132
Jacob Samaniego <jacobsmaniego@gmail.com>
2025-01-03 10:48:32 -0600

    Add fake fun fact function

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
 const nouns = ['Lava is', 'New York is', 'Dogs are', 'Cats are', 'The ocean is', 'Space is', 'Earth is', 'Billionares are', 'Fruit is']
 const adjectives = ['green', 'super', 'blue','red', 'big', 'small', 'silly', 'special']
 const other = ['aliens.', 'and tall.', 'and mean.', 'and nice.', 'fish.', 'warriors.']
+
+const fakeFunFacts = () => {
+    const randNoun = Math.floor(Math.random() * (nouns.length + 1))
+    const randAdjective = Math.floor(Math.random() * (adjectives.length + 1))
+    const randOther = Math.floor(Math.random() * (other.length + 1))
+
+    return `Fun Fact: ${nouns[randNoun]} ${adjectives[randAdjective]} ${other[randOther]}`
+
+}
