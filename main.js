0a3ed433feb34974552226e674346cd54da31eef
Jacob Samaniego <jacobsmaniego@gmail.com>
2025-01-03 10:43:11 -0600

    Add lists of words

---
+++ main.js
@@ -0,0 +1,3 @@
+const nouns = ['Lava is', 'New York is', 'Dogs are', 'Cats are', 'The ocean is', 'Space is', 'Earth is', 'Billionares are', 'Fruit is']
+const adjectives = ['green', 'super', 'blue','red', 'big', 'small', 'silly', 'special']
+const other = ['aliens.', 'and tall.', 'and mean.', 'and nice.', 'fish.', 'warriors.']
